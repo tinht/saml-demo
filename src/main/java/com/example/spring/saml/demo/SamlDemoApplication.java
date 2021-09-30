16b750496ee5a79ba68e69a282cc4818dd7ad64a
tinht <trantinh.10101991@gmail.com>
2021-09-30 11:48:56 +0700

    Test

---
+++ src/main/java/com/example/spring/saml/demo/SamlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.saml.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlDemoApplication.class, args);
+	}
+
+}
